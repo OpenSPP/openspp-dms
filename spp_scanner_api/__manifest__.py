5a356a41db403041690a5610fa1352ff12dee759
Jeremi Joslin <jeremi@newlogic.com>
2023-01-26 09:04:33 +0700

    update manifests to 15.0.0.0.0

--- spp_scanner_api/__manifest__.py
+++ spp_scanner_api/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SPP Registry: Scanner API",
     "category": "OpenSPP",
-    "version": "15.0.0.0.1",
+    "version": "15.0.0.0.0",
     "sequence": 1,
     "author": "OpenSPP.org",
     "website": "https://github.com/openspp/openspp-registry",
