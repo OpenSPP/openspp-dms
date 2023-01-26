02d4338b6ae4f3a31d361a46310f0ac0f71f842f
Jeremi Joslin <jeremi@newlogic.com>
2023-01-26 09:04:33 +0700

    [ADD] setup.py

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-openspp-openspp-dms",
+    description="Meta package for openspp-openspp-dms Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo-addon-spp_scanner_api>=15.0dev,<15.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 15.0',
+    ]
+)
