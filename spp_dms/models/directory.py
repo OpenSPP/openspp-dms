a2d16706103553ccde9153ea8fab1e484a27ee17
Edwin Gonzales <edwin@newlogic.com>
2023-05-31 18:13:28 +0800

    Remove redundant codes

--- spp_dms/models/directory.py
+++ spp_dms/models/directory.py
@@ -3,7 +3,6 @@ import logging
 from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
-# from ..tools.file import check_name
 from odoo.addons.dms.tools.file import check_name
 
 _logger = logging.getLogger(__name__)
@@ -18,34 +17,24 @@ class DmsDirectory(models.Model):
             if self.env.context.get("check_name", True) and not check_name(record.name):
                 raise ValidationError(_("The directory name is invalid."))
 
+            domain = [
+                ("id", "!=", record.id),
+                ("complete_name", "=", record.name),
+            ]
+
             if record.is_root_directory:
                 storage_id = record.sudo().storage_id.id
-                childs = (
-                    self.env["dms.directory"]
-                    .sudo()
-                    .search(
-                        [
-                            ("storage_id", "=", storage_id),
-                            ("id", "!=", record.id),
-                            ("complete_name", "=", record.name),
-                        ]
-                    )
-                )
+                domain.append(("storage_id", "=", storage_id))
             else:
                 parent_id = record.sudo().parent_id.id
-                childs = (
-                    self.env["dms.directory"]
-                    .sudo()
-                    .search(
-                        [
-                            ("parent_id", "=", parent_id),
-                            ("id", "!=", record.id),
-                            ("complete_name", "=", record.name),
-                        ]
-                    )
-                )
+                domain.append(("parent_id", "=", parent_id))
 
-            _logger.debug("DEBUG: _check_name childs: %s" % childs.sudo().name_get())
+            childs = self.env["dms.directory"].sudo().search(domain)
+
+            _logger.debug(
+                "DEBUG: _check_name childs: %s, domain: %s"
+                % (childs.sudo().name_get(), domain)
+            )
             if childs:
                 raise ValidationError(
                     _("A directory with the same name already exists.")
